b1309d9a9c9902de426db1cabb62c6a73982524a
Chad Cunningham <chadcunningham7676@gmail.com>
2020-12-04 23:26:37 -0500

    COmpleted Sluggish Octopus method

--- Octopus_problems/problems.rb
+++ Octopus_problems/problems.rb
@@ -9,3 +9,15 @@ end
 fish = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
 
 p biggest_fish(fish)
+
+def sluggish_octopus(fish)
+  biggest = fish.first
+  fish.each do |f1|
+    fish.each do |f2|
+      biggest = f1 if f1.length >= f2.length && f1.length >= biggest.length
+    end
+  end
+  biggest
+end
+
+p sluggish_octopus(fish)
