e1c598315a3d25d18e6937e1a66b09a9d6c44791
Chad Cunningham <chadcunningham7676@gmail.com>
2020-12-04 23:50:05 -0500

    Completed slow_dance method

--- Octopus_problems/problems.rb
+++ Octopus_problems/problems.rb
@@ -52,3 +52,14 @@ def fish_merge(left, right)
 end
 
 p dominant_octopus(fish)
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(direction, tiles_array)
+  tiles_array.each_with_index do |tile, i|
+    return i if tile == direction
+  end
+end
+
+p slow_dance("up", tiles_array)
+p slow_dance("right-down", tiles_array)
