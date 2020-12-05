16d1e0a1f4e7476420fea9844ffefb82a9b612c8
Chad Cunningham <chadcunningham7676@gmail.com>
2020-12-04 23:54:10 -0500

    COmpleted Constant_dance! method

--- Octopus_problems/problems.rb
+++ Octopus_problems/problems.rb
@@ -63,3 +63,21 @@ end
 
 p slow_dance("up", tiles_array)
 p slow_dance("right-down", tiles_array)
+
+tiles_hash = {
+    "up" => 0, 
+    "right-up" => 1, 
+    "right" => 2, 
+    "right-down" => 3, 
+    "down" => 4, 
+    "left-down" => 5, 
+    "left" => 6,  
+    "left-up" => 7
+}
+
+def constant_dance!(direction, tiles_hash)
+  tiles_hash[direction]
+end
+
+p constant_dance!("up", tiles_hash)
+p constant_dance!("right-down", tiles_hash)
