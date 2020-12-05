d1c29f06d1d77c694ca98a68a5f65f0093ae7ea7
Chad Cunningham <chadcunningham7676@gmail.com>
2020-12-04 23:44:38 -0500

    Completed dominant octopus method

--- Octopus_problems/problems.rb
+++ Octopus_problems/problems.rb
@@ -21,3 +21,34 @@ def sluggish_octopus(fish)
 end
 
 p sluggish_octopus(fish)
+
+def dominant_octopus(fish)
+  merge_sort(fish).last
+end
+
+def merge_sort(fish)
+  return fish if fish.length <= 1
+  mid = fish.length / 2
+  fish_merge(
+      merge_sort(fish.take(mid)),
+      merge_sort(fish.drop(mid))
+  )
+end
+
+def fish_merge(left, right)
+  merged_array = []
+  prc = Proc.new { |fish1, fish2| fish1.length <=> fish2.length }
+  until left.empty? || right.empty?
+    case prc.call(left.first, right.first)
+    when -1 
+      merged_array << left.shift
+    when 0
+      merged_array << left.shift
+    when 1
+      merged_array << right.shift
+    end
+  end
+  merged_array + left + right
+end
+
+p dominant_octopus(fish)
