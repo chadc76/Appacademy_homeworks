857399c956c4fbd96c753cfa0c9e2faf133a57f0
Chad Cunningham <chadcunningham7676@gmail.com>
2021-01-18 17:11:58 -0500

    Completed LRUCache#initialize, LRUCache#count, LRUCache#add methods and helper methods

--- LRU_Cache/lru_cache.rb
+++ LRU_Cache/lru_cache.rb
@@ -1,13 +1,22 @@
 class LRUCache
-  def initialize
+  
+  attr_reader :cache_size, :cache
+  def initialize(cache_size)
+    @cache_size = cache_size
+    @cache = []
   end
   
   def count
     # returns number of elements currently in cache
+    @cache.count
   end
   
   def add(el)
     # adds element to cache according to LRU principle
+    delete_lru if self.count == cache_size && !cache.include?(el)
+    remove_el(el) if cache.include?(el)
+    @cache << el
+    true
   end
   
   def show
@@ -16,5 +25,13 @@ class LRUCache
   
   private
   # helper methods go here!
+
+  def delete_lru
+    @cache.shift
+  end
+
+  def remove_el(el)
+    @cache.delete(el)
+  end
   
 end
