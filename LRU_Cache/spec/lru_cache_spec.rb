db3c3944394df45c1f7f8ef9eef39765a718cc9c
Chad Cunningham <chadcunningham7676@gmail.com>
2021-01-18 18:01:41 -0500

    Completed spec for LRUCache#add method

--- LRU_Cache/spec/lru_cache_spec.rb
+++ LRU_Cache/spec/lru_cache_spec.rb
@@ -16,13 +16,67 @@ describe LRUCache do
   describe "count" do
     it "should return the number of objects in cache" do 
       expect(johnny_cache.count).to eq(0)
+      johnny_cache.add("I walk the line")
       johnny_cache.add(5)
-      expect(johnny_cache.count).to eq(1)
-      johnny_cache.add(1)
-      johnny_cache.add(2)
-      johnny_cache.add(3)
-      johnny_cache.add(4)
+      expect(johnny_cache.count).to eq(2)
+      johnny_cache.add([1,2,3])
+      johnny_cache.add(-5)
       expect(johnny_cache.count).to eq(4)
+    end
+  end
+
+  describe "add" do 
+
+    context "when given a new object" do
+      it "should add new object to end" do
+        expect(johnny_cache.cache).to eq([])
+        johnny_cache.add("I walk the line")
+        expect(johnny_cache.cache).to eq(["I walk the line"])
+        johnny_cache.add(5)
+        johnny_cache.add([1,2,3])
+        johnny_cache.add(-5)
+        expect(johnny_cache.cache).to eq(["I walk the line", 5, [1,2,3], -5])
+      end
+
+      it "should never have a make count larger then cache_size" do
+        expect(johnny_cache.count).to eq(0)
+        johnny_cache.add("I walk the line")
+        johnny_cache.add(5)
+        johnny_cache.add([1,2,3])
+        johnny_cache.add(-5)
+        expect(johnny_cache.count).to eq(4)
+        johnny_cache.add({a: 1, b: 2, c: 3})
+        expect(johnny_cache.count).to eq(4)
+      end
+
+      context "when cache is at capacity" do 
+        it "should remove the LRU object, and add new object to the end" do
+          johnny_cache.add("I walk the line")
+          johnny_cache.add(5)
+          johnny_cache.add([1,2,3])
+          johnny_cache.add(-5)
+          johnny_cache.add({a: 1, b: 2, c: 3})
+          expect(johnny_cache.cache).to eq([5, [1,2,3], -5, {a: 1, b: 2, c: 3} ])
+        end
+      end
+    end
+
+    context "when given an object already in cache" do 
+      it "Should not add the object twice" do
+        johnny_cache.add("I walk the line")
+        johnny_cache.add(5)
+        johnny_cache.add(5)
+        expect(johnny_cache.cache.count(5)).to eq(1)
+      end
+
+      it "should move the object to the end of the cache" do
+        johnny_cache.add("I walk the line")
+        johnny_cache.add(5)
+        johnny_cache.add([1,2,3])
+        expect(johnny_cache.cache).to eq(["I walk the line", 5, [1,2,3]])
+        johnny_cache.add(5)
+        expect(johnny_cache.cache).to eq(["I walk the line",[1,2,3], 5])
+      end
     end
   end
 end
