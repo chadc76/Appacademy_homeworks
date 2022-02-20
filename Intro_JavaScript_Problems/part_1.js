ddb76f175f4effdf90107eda62d8f06d9b3e1704
Chad Cunningham <chadcunningham7676@gmail.com>
2022-02-20 15:19:33 -0500

    Added FizzBuzz solution

--- Intro_JavaScript_Problems/part_1.js
+++ Intro_JavaScript_Problems/part_1.js
@@ -10,3 +10,19 @@ const isSubstring = (searchString, subString) => {
 
 isSubstring("time to program", "time");
 isSubstring("Jump for joy", "joys");
+
+
+// Phase II - JS Looping Constructs
+
+const fizzBuzz = (array) => {
+  let newArr = array.filter(
+    num => {
+      if ((num % 3 === 0 || num % 5 === 0) && num % 15 != 0){
+        return num;
+      };
+    }
+  )
+  console.log(newArr);
+};
+
+fizzBuzz([1,3,4,5,9,10,12,15,30,60]);
