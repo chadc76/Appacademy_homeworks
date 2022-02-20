29682ad2986eb9f424a4a2627fd654eaaca7a215
Chad Cunningham <chadcunningham7676@gmail.com>
2022-02-20 16:23:58 -0500

    Completed Phase II: Constructors, Prototypes &this

--- Intro_JavaScript_Problems/part_2.js
+++ Intro_JavaScript_Problems/part_2.js
@@ -10,3 +10,40 @@ const printCallback = (arr) => {
 };
 
 titleize(["Mary", "Brian", "Leo"], printCallback);
+
+
+// Phase II: Constructors, Prototypes, and this
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function() {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function() {
+  let rand =  Math.floor(Math.random() * this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[rand]}`);
+};
+
+const cecil = new Elephant("Cecil", 500, ['jumping', 'stomping']);
+
+cecil.trumpet();
+console.log(cecil.height);
+cecil.grow();
+console.log(cecil.height);
+console.log(cecil.tricks);
+cecil.addTrick("Singing");
+console.log(cecil.tricks);
+cecil.play();
