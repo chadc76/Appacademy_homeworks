c114cdb05e14137ff87ac1aaadd966d7c3581c81
Chad Cunningham <chadcunningham7676@gmail.com>
2022-02-20 15:35:56 -0500

    Completed Phase II - JS Looping Constructs

--- Intro_JavaScript_Problems/part_1.js
+++ Intro_JavaScript_Problems/part_1.js
@@ -29,18 +29,37 @@ fizzBuzz([1,3,4,5,9,10,12,15,30,60]);
 
 
 const isPrime = (num) => {
-  let i = 2
+  if (num < 2){ return false; }
+  let i = 2;
   while (i < num ) {
     if (num % i == 0){
-      console.log(false)
-      return
+      return false;
     }
     i++
   }
-  console.log(true)
+  return true;
 }
 
-isPrime(2);
-isPrime(10);
-isPrime(15485863);
-isPrime(3548563);
+console.log(isPrime(2));
+console.log(isPrime(10));
+console.log(isPrime(15485863));
+console.log(isPrime(3548563));
+
+
+const sumOfNPrimes = (n) => {
+  let primes = 0;
+  let i = 1;
+  let sum = 0;
+  while (primes < n) {
+    if (isPrime(i)){
+      sum += i
+      primes++
+    }
+    i++
+  }
+  console.log(sum);
+};
+
+sumOfNPrimes(0);
+sumOfNPrimes(1);
+sumOfNPrimes(4);
