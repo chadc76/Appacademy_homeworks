e1f8561113cb1c2f393ba2287f9a77f50d4ea8f6
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-13 21:06:09 -0400

    Compelted Phase II: Draw a Rectangle

--- canvas_practice/practice.js
+++ canvas_practice/practice.js
@@ -3,4 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
   canvas.style.width = 500;
   canvas.style.height = 500;
   const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = 'red';
+  void ctx.fillRect(0, 0, 500, 500);
 });
