9af3b4383a0c892a07636442373adf789fe72eda
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-13 21:03:18 -0400

    Completed Phase I: Setting up your Canvas Element

--- canvas_practice/practice.js
+++ canvas_practice/practice.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+  const canvas = document.getElementById('mycanvas');
+  canvas.style.width = 500;
+  canvas.style.height = 500;
+  const ctx = canvas.getContext('2d');
 });
