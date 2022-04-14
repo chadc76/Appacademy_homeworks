211a8e981f79fd196bfba27a353e9afae4ea1812
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-13 22:33:58 -0400

    Changed shape to Smiley

--- canvas_practice/practice.js
+++ canvas_practice/practice.js
@@ -1,17 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function smiley() {
   const canvas = document.getElementById('mycanvas');
-  canvas.style.width = 500;
-  canvas.style.height = 500;
+  canvas.width = 500;
+  canvas.height = 500;
 
   const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'red';
+  ctx.fillStyle = 'purple';
   ctx.fillRect(0, 0, 500, 500);
 
   ctx.beginPath();
-  ctx.arc(100, 100, 20, 0, 2*Math.PI);
+  ctx.arc(250, 250, 240, 0, 2 * Math.PI);
+  ctx.fillStyle = 'blue';
+  ctx.fill();
+  ctx.moveTo(375, 250);
+  ctx.arc(250, 250, 125, 0, Math.PI, false);  // Mouth (clockwise)
+  ctx.moveTo(205, 160);
+  ctx.arc(180, 160, 25, 0, Math.PI * 2, true);  // Left eye
+  ctx.moveTo(335, 160);
+  ctx.arc(310, 160, 25, 0, Math.PI * 2, true);  // Right eye
   ctx.strokeStyle = 'black'
   ctx.lineWidth = 5;
   ctx.stroke();
-  ctx.fillStyle = 'blue';
-  ctx.fill();
 });
